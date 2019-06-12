d8b4972496b26a661044bd4981344fe00432cbda
cwylie0 <cwylie0@hotmail.com>
2019-06-12 17:27:32 -0400

    Moved Python files

--- rating/headerTest.py
+++
@@ -1,9 +0,0 @@
-def printHeader(SocialNetwork):
-    print("\n")
-    header = SocialNetwork + " Ratings"
-    print(header)
-    print('-' * len(header))   
-
-
-printHeader("Test")
-printHeader("Polyphonic")
