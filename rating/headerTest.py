b15655fce910287a306a239e072ff21ebb79c32d
cwylie0 <cwylie0@hotmail.com>
2019-06-12 16:57:31 -0400

    Updated all files

---
+++ rating/headerTest.py
@@ -0,0 +1,9 @@
+def printHeader(SocialNetwork):
+    print("\n")
+    header = SocialNetwork + " Ratings"
+    print(header)
+    print('-' * len(header))   
+
+
+printHeader("Test")
+printHeader("Polyphonic")
